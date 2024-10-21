91835d2210423f48edbf6e010482f3011353212b
DanPancho <daarcosa@puce.edu.ec>
2024-10-20 22:55:51 -0500

    (master) initial commit

--- src/main/java/com/example/pruebatecnica/controller/MovimientoController.java
+++ src/main/java/com/example/pruebatecnica/controller/MovimientoController.java
@@ -1,7 +1,6 @@
 package com.example.pruebatecnica.controller;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
